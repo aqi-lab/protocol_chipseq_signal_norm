d90ffcdd5238e19c1f5ffa09452224c3fd52414e
aqi-lab <qianthony2006@gmail.com>
2025-07-21 11:41:30 -0400

    Fixed bug at line 10
    eval "local arr_siz_2=\${#${arr_nam_2}[@]}"

---
+++ scripts/check_arrays_lengths.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#  Function to validate that two arrays have matching lengths
+function check_arrays_lengths() {
+    local arr_nam_1="${1}"
+    local arr_nam_2="${2}"
+
+    #  Get array sizes using indirect references
+    eval "local arr_siz_1=\${#${arr_nam_1}[@]}"
+    eval "local arr_siz_2=\${#${arr_nam_2}[@]}"
+
+    # shellcheck disable=SC2154
+    if [[ "${arr_siz_1}" -ne "${arr_siz_2}" ]]; then
+        echo \
+            "Error: '${arr_nam_1}' must match the number of '${arr_nam_2}'." \
+            "Got '${arr_siz_1}' for '${arr_nam_1}' but '${arr_siz_2}' for" \
+            "'${arr_nam_2}'." >&2
+        return 1
+    fi
+}
