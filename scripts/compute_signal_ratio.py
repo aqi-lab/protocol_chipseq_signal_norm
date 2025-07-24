2bc2850ccc6291ec388541acc3e81b3d192c286d
aqi-lab <qianthony2006@gmail.com>
2025-07-24 12:10:03 -0400

    Update compute_signal_ratio.py
    added 2m

--- scripts/compute_signal_ratio.py
+++ scripts/compute_signal_ratio.py
@@ -176,7 +176,7 @@ def convert_rom_int(rom):
     dct_rom = {
         "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, 
         "VIII": 8, "IX": 9, "X": 10, "XI": 11, "XII": 12, "XIII": 13, 
-        "XIV": 14, "XV": 15, "XVI": 16
+        "XIV": 14, "XV": 15, "XVI": 16, "2m": 17
     }
     return dct_rom.get(rom, float("inf"))  # Assign a high value if unknown
 
