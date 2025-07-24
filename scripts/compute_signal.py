4e2786b423edfa5c5bb6419a873c9d8e93e1731b
aqi-lab <qianthony2006@gmail.com>
2025-07-24 12:09:42 -0400

    Update compute_signal.py
    added 2m

--- scripts/compute_signal.py
+++ scripts/compute_signal.py
@@ -104,7 +104,7 @@ interactive = False
 roman_to_int = {
     "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, 
     "VIII": 8, "IX": 9, "X": 10, "XI": 11, "XII": 12, "XIII": 13, 
-    "XIV": 14, "XV": 15, "XVI": 16
+    "XIV": 14, "XV": 15, "XVI": 16, "2m": 17
 }
 
 
